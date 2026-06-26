4074c3334c69c51fa8959eb9c929a6b268a4183c
observatoriopsicocriminal-eng <observatoriopsicocriminal@gmail.com>
2026-06-26 12:00:38 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// Enlace con inyección dinámica para forzar la actualización en tiempo real
 const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS8IXB_orCVp5KMu4bCoYhXnPkvriO49i7DX2S59u4HSKaoQudOkEtvPDMXcDRfbnvTP3y2VppuAlh0/pub?output=csv&cachebust=" + new Date().getTime();
