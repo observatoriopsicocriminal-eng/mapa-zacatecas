80441229c3c6a5f4a98f9cb7f060aa4f22255037
observatoriopsicocriminal-eng <observatoriopsicocriminal@gmail.com>
2026-06-28 07:05:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
+JavaScript
+// config.js - Configuración Cartográfica del Observatorio Psicocriminal
+
+const MAP_CONFIG = {
+    center: [22.7709, -102.5832], // Centro geográfico del Estado de Zacatecas
+    zoom: 8,
+    minZoom: 7,
+    maxZoom: 14,
+    bounds: [
+        [21.0, -104.5], // Límite Suroeste
+        [25.0, -100.5]  // Límite Noreste
+    ]
+};
+
+// Enlace limpio a Google Sheets con actualización forzada de datos
 const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS8IXB_orCVp5KMu4bCoYhXnPkvriO49i7DX2S59u4HSKaoQudOkEtvPDMXcDRfbnvTP3y2VppuAlh0/pub?output=csv&cachebust=" + new Date().getTime();
